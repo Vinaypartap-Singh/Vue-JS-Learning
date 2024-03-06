0c13ac6d6bdeafaeb48699a9984c902cbfae694d
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2024-03-06 10:26:52 -0500

    Vue JS CLI Setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script scoped>
+export default {
+  data() {
+    return {
+      title: "Vue JS CLI 3"
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
